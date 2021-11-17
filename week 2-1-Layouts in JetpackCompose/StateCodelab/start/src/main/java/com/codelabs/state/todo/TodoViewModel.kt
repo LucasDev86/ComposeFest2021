6943d3813857b2b79f823aa029e240ff97b3745b
B2110353-F5\BGF <juseyo86@naver.com>
2021-11-17 09:55:26 +0900

    feat : complete

--- week 2-1-Layouts in JetpackCompose/StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
+++ week 2-1-Layouts in JetpackCompose/StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
@@ -26,28 +26,38 @@ import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
 
-    // remove the LiveData and replace it with a mutableStateListOf
-    //private var _todoItems = MutableLiveData(listOf<TodoItem>())
-    //val todoItems: LiveData<List<TodoItem>> = _todoItems
-
-    // private state
     private var currentEditPosition by mutableStateOf(-1)
 
-    // state: todoItems
     var todoItems = mutableStateListOf<TodoItem>()
         private set
 
-    // state
     val currentEditItem: TodoItem?
         get() = todoItems.getOrNull(currentEditPosition)
 
-    // event: addItem
     fun addItem(item: TodoItem) {
         todoItems.add(item)
     }
 
-    // event: removeItem
     fun removeItem(item: TodoItem) {
         todoItems.remove(item)
+        onEditDone() // don't keep the editor open when removing items
     }
+
+    fun onEditItemSelected(item: TodoItem) {
+        currentEditPosition = todoItems.indexOf(item)
+    }
+
+    fun onEditDone() {
+        currentEditPosition = -1
+    }
+
+    fun onEditItemChange(item: TodoItem) {
+        val currentItem = requireNotNull(currentEditItem)
+        require(currentItem.id == item.id) {
+            "You can only change an item with the same id as currentEditItem"
+        }
+
+        todoItems[currentEditPosition] = item
+    }
+
 }
